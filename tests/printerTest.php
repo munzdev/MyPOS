b8407a5d10a883aa21a1d94f1f7d6860fea056f7
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-07-22 10:24:02 +0200

    moved test file to a new "tests" directory

---
+++ tests/printerTest.php
@@ -0,0 +1,19 @@
+<?php
+/* Call this file 'hello-world.php' */
+require __DIR__ . '/../src/vendor/autoload.php';
+
+use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
+use Mike42\Escpos\Printer;
+
+$o_connector = new NetworkPrintConnector("192.168.0.50", 9100);
+$o_printer = new Printer($o_connector);
+
+try
+{
+    $o_printer->text("Hello World!\n This is a Test!\n\n");
+    $o_printer->cut();
+}
+finally
+{
+    $o_printer->close();
+}
