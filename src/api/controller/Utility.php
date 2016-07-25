74361e66543e771612a71de4cd563972e995ce8f
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-07-25 18:31:26 +0200

    doen't return PRINTER_ constants ( may have hex-decimal encoded values which doesn't work correctly with json_encode

--- src/api/controller/Utility.php
+++ src/api/controller/Utility.php
@@ -5,30 +5,33 @@ use Lib\Controller;
 
 class Utility extends Controller
 {
-	public function KeepSessionAliveAction()
-	{
-		return true;
-	}
+    public function KeepSessionAliveAction()
+    {
+        return true;
+    }
 
-	public function ConstantsAction()
-	{
-		$a_defined_constants = get_defined_constants(TRUE)['user'];
+    public function ConstantsAction()
+    {
+        $a_defined_constants = get_defined_constants(TRUE)['user'];
 
-		$a_js_constants = array();
+        $a_js_constants = array();
 
-		foreach ($a_defined_constants as $str_name => $value)
-		{
-			$str_namespace = "mypos\\";
-			$i_start = strlen($str_namespace);
+        foreach ($a_defined_constants as $str_name => $value)
+        {
+            $str_namespace = "mypos\\";
+            $i_start = strlen($str_namespace);
 
-			if (0 === strpos($str_name, $str_namespace))
-			{
-				$str_name = substr($str_name, $i_start);
-				$a_js_constants[$str_name] = $value;
-			}
+            if (0 === strpos($str_name, $str_namespace))
+            {
+                $str_name = substr($str_name, $i_start);
 
-		}
+                if(0 === strpos($str_name, "PRINTER_"))
+                    continue;
 
-		return $a_js_constants;
-	}
+                $a_js_constants[$str_name] = $value;
+            }
+        }
+
+        return $a_js_constants;
+    }
 }
