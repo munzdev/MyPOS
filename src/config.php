28408efb5ea7124f1ebe90f436cac3abaa902b09
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-07-27 19:57:40 +0200

    don't use peristent connection. Creates Problems with temp tables

--- src/config.php
+++ src/config.php
@@ -8,7 +8,7 @@ $a_config['DB']['Host'] = 'localhost';
 $a_config['DB']['User'] = 'root';
 $a_config['DB']['Password'] = '';
 $a_config['DB']['Database'] = 'mypos';
-$a_config['DB']['Persistent'] = true;
+$a_config['DB']['Persistent'] = false;
 
 $a_config['Auth']['RememberMe_PrivateKey'] = 'MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAfmBSwS0WmfKNW1Dq2N4MJ4gYDApG6lW19QhvDp2g80ajw74D2Xijm4rIuxaJPJ64GazdNWUHuc+1CL5eEkBopwIDAQAB';
 
