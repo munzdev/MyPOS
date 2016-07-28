e820fa863d969e4cd431a1829058af30434a98c2
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-07-28 09:57:48 +0200

    - fixed UnloadWebsite method - fixed code formation

--- src/public/js/MyPOS.js
+++ src/public/js/MyPOS.js
@@ -2,103 +2,104 @@
 // ----------
 
 //Includes file dependencies
-define(["app", "underscore", "jquery"], function(app) {
-	"use strict";
+define(["app", "underscore", "jquery"], function(app)
+{
+    "use strict";
 
-	var MyPOS = {};
+    var MyPOS = {};
 
-	MyPOS.RenderPageTemplate = function(View, Name, Template, Datas)
-	{
-	    // Sets the view's template property
-            View.template = _.template(Template);
+    MyPOS.RenderPageTemplate = function(View, Name, Template, Datas)
+    {
+        // Sets the view's template property
+        View.template = _.template(Template);
 
-            var page = $('#' + Name);
+        var page = $('#' + Name);
 
-            if(page.length > 0) {
-                    page.remove();
-            }
-
-		//append the new page onto the end of the body
-	    View.$el.append('<div data-role="page" id="' + Name + '">' + View.template(Datas) + '</div>');
-
-		//initialize the new page
-	    $.mobile.initializePage();
-
-	};
-
-	MyPOS.RenderDialogeTemplate = function(View, Name, Template, Datas)
-	{
-	    // Sets the view's template property
-            View.template = _.template(Template);
-
-            var page = $('#' + Name);
-
-            if(page.length > 0) {
-                    page.html(View.template(Datas));
-            } else {
-                    //append the new page onto the end of the body
-                var dialoge = View.$el.append('<div data-role="page" id="' + Name + '" data-dialog="true" data-close-btn="none">' + View.template(Datas) + '</div>');
-
-                //initialize the new page
-                $.mobile.initializePage();
-            }
-	};
-
-        MyPOS.RenderPopupTemplate = function(View, Name, Template, Datas, Extras)
-	{
-	    // Sets the view's template property
-            View.template = _.template(Template);
-
-            var page = $('#' + Name);
-
-            if(page.length > 0) {
-                    page.html(View.template(Datas));
-            } else {
-                //append the new page onto the end of the body
-                var popup = View.$el.append('<div data-role="popup" id="' + Name + '" ' + Extras + '>' + View.template(Datas) + '</div>');
-
-                //initialize the new page
-                $.mobile.initializePage();
-            }
-	};
-
-	MyPOS.ChangePage = function(View, options)
-	{
-		Backbone.history.navigate(View, true);
-	}
-
-	MyPOS.UnloadWebsite = function(result)
-	{
-		location.reload(true);
-	}
-
-	MyPOS.DisplayError = function(errorMessage)
-	{
-		alert(errorMessage);
-	}
-
-	MyPOS.KeepSessionAlive = function()
-	{
-		$.ajax({url: app.API + 'Utility/KeepSessionAlive/',
-	        	dataType: 'json'
-	    });
-	}
-
-        MyPOS.DateFromMysql = function(mysql_string)
-        {
-            var t, result = null;
-
-            if( typeof mysql_string === 'string' )
-            {
-                t = mysql_string.split(/[- :]/);
-
-                //when t[3], t[4] and t[5] are missing they defaults to zero
-                result = new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
-            }
-
-           return result;
+        if(page.length > 0) {
+            page.remove();
         }
 
-	return (window.MyPOS = MyPOS);
+            //append the new page onto the end of the body
+        View.$el.append('<div data-role="page" id="' + Name + '">' + View.template(Datas) + '</div>');
+
+            //initialize the new page
+        $.mobile.initializePage();
+
+    };
+
+    MyPOS.RenderDialogeTemplate = function(View, Name, Template, Datas)
+    {
+        // Sets the view's template property
+        View.template = _.template(Template);
+
+        var page = $('#' + Name);
+
+        if(page.length > 0) {
+            page.html(View.template(Datas));
+        } else {
+            //append the new page onto the end of the body
+            var dialoge = View.$el.append('<div data-role="page" id="' + Name + '" data-dialog="true" data-close-btn="none">' + View.template(Datas) + '</div>');
+
+            //initialize the new page
+            $.mobile.initializePage();
+        }
+    };
+
+    MyPOS.RenderPopupTemplate = function(View, Name, Template, Datas, Extras)
+    {
+        // Sets the view's template property
+        View.template = _.template(Template);
+
+        var page = $('#' + Name);
+
+        if(page.length > 0) {
+            page.html(View.template(Datas));
+        } else {
+            //append the new page onto the end of the body
+            var popup = View.$el.append('<div data-role="popup" id="' + Name + '" ' + Extras + '>' + View.template(Datas) + '</div>');
+
+            //initialize the new page
+            $.mobile.initializePage();
+        }
+    };
+
+    MyPOS.ChangePage = function(View, options)
+    {
+        Backbone.history.navigate(View, true);
+    }
+
+    MyPOS.UnloadWebsite = function(result)
+    {
+        $(location).attr('href', app.URL);
+    }
+
+    MyPOS.DisplayError = function(errorMessage)
+    {
+        alert(errorMessage);
+    }
+
+    MyPOS.KeepSessionAlive = function()
+    {
+        $.ajax({url: app.API + 'Utility/KeepSessionAlive/',
+            dataType: 'json'
+        });
+    }
+
+    MyPOS.DateFromMysql = function(mysql_string)
+    {
+        var t, result = null;
+
+        if( typeof mysql_string === 'string' )
+        {
+            t = mysql_string.split(/[- :]/);
+
+            //when t[3], t[4] and t[5] are missing they defaults to zero
+            result = new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
+        }
+
+       return result;
+    }
+
+    return (window.MyPOS = MyPOS);
 
 } );
