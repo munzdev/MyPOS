5b379242d5f2bc3476d8560da515fc8006057f23
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-07-28 10:27:47 +0200

    fixed formating of code and fixed login function (render() needs to bind to view)

--- src/public/js/views/LoginView.js
+++ src/public/js/views/LoginView.js
@@ -2,8 +2,9 @@
 // =============
 
 // Includes file dependencies
-define([ "app", "MyPOS", 'text!templates/dialoges/login.phtml'], function( app, MyPOS, Template ) {
-	"use strict";
+define([ "app", "MyPOS", 'text!templates/dialoges/login.phtml'],
+function( app, MyPOS, Template ) {
+    "use strict";
 
     // Extends Backbone.View
     var LoginView = Backbone.View.extend( {
@@ -11,31 +12,31 @@ define([ "app", "MyPOS", 'text!templates/dialoges/login.phtml'], function( app, 
     	title: 'login',
     	el: 'body',
         events: {
-        	'click #login-submit': 'doLogin',
+            'click #login-submit': 'doLogin',
             'keyup #password': 'onPasswordKeyup'
         },
 
         // The View Constructor
         initialize: function() {
-           // _.bindAll(this);
+            _.bindAll(this, "render");
 
-        	// Listen for session logged_in state changes and re-render
-        	app.session.on("change:logged_in", this.render);
+            // Listen for session logged_in state changes and re-render
+            app.session.on("change:logged_in", this.render);
 
-        	if(app.session.get('logged_in'))
-    		{
-        		MyPOS.ChangePage("#order-overview");
-        		return;
-    		}
+            if(app.session.get('logged_in'))
+            {
+                MyPOS.ChangePage("#order-overview");
+                return;
+            }
 
-    		this.render();
+            this.render();
         },
 
         // Renders all of the Category models on the UI
         render: function() {
-        	MyPOS.RenderDialogeTemplate(this, this.title, Template);
+            MyPOS.RenderDialogeTemplate(this, this.title, Template);
 
-        	return this;
+            return this;
         },
 
         // Allow enter press to trigger login
@@ -51,28 +52,31 @@ define([ "app", "MyPOS", 'text!templates/dialoges/login.phtml'], function( app, 
             }
         },
 
-        doLogin: function(evt) {
-        	 if(evt) evt.preventDefault();
+        doLogin: function(event) {
+            if(event)
+                event.preventDefault();
 
-             if(this.$("#username").val() != '' && this.$("#password").val() != '') {
-                 app.session.login({
-                     username: this.$("#username").val(),
-                     password: this.$("#password").val(),
-                     rememberMe: this.$("#rememberMe").val()
-                 },
-                 function(user) {
-                	 if(DEBUG) console.log("SUCCESS", user);
-                	 MyPOS.ChangePage("#order-overview");
-                 },
-                 function(result) {
-                     if(DEBUG) console.log("ERROR", result);
-                     app.error.showAlert('Login fehler!', 'Login ist fehlgeschlagen!');
-                 });
-             } else {
-                 // Invalid clientside validations thru parsley
-                 if(DEBUG) console.log("Did not pass clientside validation");
-
-             }
+            if(this.$("#username").val() != '' && this.$("#password").val() != '')
+            {
+                app.session.login({
+                    username: this.$("#username").val(),
+                    password: this.$("#password").val(),
+                    rememberMe: this.$("#rememberMe").val()
+                },
+                function(user) {
+                    if(DEBUG) console.log("SUCCESS", user);
+                    MyPOS.ChangePage("#order-overview");
+                },
+                function(result) {
+                    if(DEBUG) console.log("ERROR", result);
+                    app.error.showAlert('Login fehler!', 'Login ist fehlgeschlagen!');
+                });
+            }
+            else
+            {
+                // Invalid clientside validations thru parsley
+                if(DEBUG) console.log("Did not pass clientside validation");
+            }
         }
 
     } );
