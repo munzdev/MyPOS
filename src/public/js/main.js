5342c4a3fa5f2458792082d4e81c76826cd1281b
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-09-07 10:50:46 +0200

    removed wrong router

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -49,7 +49,7 @@ require([ "app",
           "Webservice",
           "websocket/Chat",
           "websocket/API",
-          "routers/BasicRouter",
+          "routers/GlobalRouter",
           "routers/AdminRouter",
           "models/SessionModel",
           "views/dialoges/ErrorDialogView",
@@ -60,7 +60,7 @@ require([ "app",
             Webservice,
             WsChat,
             WsAPI,
-            BasicRouter,
+            GlobalRouter,
             AdminRouter,
             SessionModel,
             ErrorDialogView) {
@@ -122,7 +122,7 @@ require([ "app",
 
     // Instantiates a new Backbone.js Mobile Router
     app.routers = {};
-    app.routers.basic = new BasicRouter();
+    app.routers.global = new GlobalRouter();
     app.routers.admin = new AdminRouter();
 
     // Error Displaying
