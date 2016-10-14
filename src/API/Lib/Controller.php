17ad97dc35ae1ac034691e05768123473321efe4
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-10-14 19:26:00 +0200

    added missing files (got deletted

---
+++ src/API/Lib/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace API\Lib;
+
+use Psr\Log\LoggerInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+abstract class Controller
+{
+    protected $o_app;
+    protected $o_logger;
+    
+    public function __construct($o_app, LoggerInterface $o_logger)
+    {
+        $this->o_app = $o_app;
+        $this->o_logger = $o_logger;
+    }
+    
+    public abstract function __invoke(Request $request, Response $response, $args);
+}
---
+++ src/API/bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+// To help the built-in PHP dev server, check if the request was actually for
+// something which should probably be served as a static file
+if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
+    return false;
+}
+
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+mb_http_input('UTF-8');
+mb_regex_encoding('UTF-8');
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require 'constants.php';
+require 'functions.php';
+
+session_start();
+
+// Instantiate the app
+$settings = require 'settings.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require 'dependencies.php';
+
+// Register middleware
+require 'middleware.php';
+
+// Register routes
+require 'routes.php';
+// Run!
+$app->run();
