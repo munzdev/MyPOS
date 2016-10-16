6066597a015459c6a483ef69c10350fc304f5f67
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-10-16 12:03:31 +0200

    added comments

--- src/API/bootstrap.php
+++ src/API/bootstrap.php
@@ -5,18 +5,21 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
 
+// Verify UTF-8 encoding is enabled
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
 mb_http_input('UTF-8');
 mb_regex_encoding('UTF-8');
 
+// Load up composers libs
 require __DIR__ . '/../vendor/autoload.php';
 
+// Load constants and functions
 require 'constants.php';
 require 'functions.php';
 
+// Enable Session and set script running time to infinite in Debug mode
 session_start();
-
 if(API\DEBUG) 
     set_time_limit(0);
 
@@ -32,5 +35,6 @@ require 'middleware.php';
 
 // Register routes
 require 'routes.php';
+
 // Run!
 $app->run();
