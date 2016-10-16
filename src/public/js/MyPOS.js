65c79718921fb658b99c5ddd12b85150761eab91
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-10-16 14:43:06 +0200

    rewrote to ES6

--- src/public/js/MyPOS.js
+++ src/public/js/MyPOS.js
@@ -2,109 +2,108 @@
 // ----------
 
 //Includes file dependencies
-define(["app", "underscore", "jquery"], function(app)
+define(["app"], function()
 {
     "use strict";
-
-    var MyPOS = {};
-
-    MyPOS.RenderPageTemplate = function(View, Name, Template, Datas)
-    {
-        // Sets the view's template property
-        View.template = _.template(Template);
-
-        var page = $('#' + Name);
-
-        if(page.length > 0) {
-            page.remove();
-        }
-
-            //append the new page onto the end of the body
-        View.$el.append('<div data-role="page" id="' + Name + '">' + View.template(Datas) + '</div>');
-
-            //initialize the new page
-        $.mobile.initializePage();
-
-    };
-
-    MyPOS.RenderDialogeTemplate = function(View, Name, Template, Datas)
-    {
-        // Sets the view's template property
-        View.template = _.template(Template);
-
-        var page = $('#' + Name);
-
-        if(page.length > 0) {
-            page.html(View.template(Datas));
-        } else {
-            //append the new page onto the end of the body
-            var dialoge = View.$el.append('<div data-role="page" id="' + Name + '" data-dialog="true" data-close-btn="none">' + View.template(Datas) + '</div>');
-
-            //initialize the new page
-            $.mobile.initializePage();
-        }
-    };
-
-    MyPOS.RenderPopupTemplate = function(View, Name, Template, Datas, Extras)
-    {
-        // Sets the view's template property
-        View.template = _.template(Template);
-
-        var page = $('#' + Name);
-
-        if(page.length > 0) {
-            page.html(View.template(Datas));
-        } else {
-            //append the new page onto the end of the body
-            var popup = View.$el.append('<div data-role="popup" id="' + Name + '" ' + Extras + '>' + View.template(Datas) + '</div>');
-
-            //initialize the new page
-            $.mobile.initializePage();
-        }
-    };
-
-    MyPOS.ChangePage = function(View, options)
-    {
-        Backbone.history.navigate(View, true);
-    }
-
-    MyPOS.ReloadPage = function()
-    {
-        Backbone.history.loadUrl();
-    }
-
-    MyPOS.UnloadWebsite = function(result)
-    {
-        $(location).attr('href', app.URL);
-    }
-
-    MyPOS.DisplayError = function(errorMessage)
-    {
-        alert(errorMessage);
-    }
-
-    MyPOS.KeepSessionAlive = function()
-    {
-        $.ajax({url: app.API + 'Utility/KeepSessionAlive/',
-            dataType: 'json'
-        });
-    }
-
-    MyPOS.DateFromMysql = function(mysql_string)
-    {
-        var t, result = null;
-
-        if( typeof mysql_string === 'string' )
+    
+    class MyPOS {
+        RenderPageTemplate(View, Name, Template, Datas)
         {
-            t = mysql_string.split(/[- :]/);
+            // Sets the view's template property
+            View.template = _.template(Template);
 
-            //when t[3], t[4] and t[5] are missing they defaults to zero
-            result = new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
+            var page = $('#' + Name);
+
+            if(page.length > 0) {
+                page.remove();
+            }
+
+                //append the new page onto the end of the body
+            View.$el.append('<div data-role="page" id="' + Name + '">' + View.template(Datas) + '</div>');
+
+                //initialize the new page
+            $.mobile.initializePage();
         }
+        
+        RenderDialogeTemplate(View, Name, Template, Datas)
+        {
+            // Sets the view's template property
+            View.template = _.template(Template);
 
-       return result;
+            var page = $('#' + Name);
+
+            if(page.length > 0) {
+                page.html(View.template(Datas));
+            } else {
+                //append the new page onto the end of the body
+                var dialoge = View.$el.append('<div data-role="page" id="' + Name + '" data-dialog="true" data-close-btn="none">' + View.template(Datas) + '</div>');
+
+                //initialize the new page
+                $.mobile.initializePage();
+            }
+        }
+        
+        RenderPopupTemplate(View, Name, Template, Datas, Extras)
+        {
+            // Sets the view's template property
+            View.template = _.template(Template);
+
+            var page = $('#' + Name);
+
+            if(page.length > 0) {
+                page.html(View.template(Datas));
+            } else {
+                //append the new page onto the end of the body
+                var popup = View.$el.append('<div data-role="popup" id="' + Name + '" ' + Extras + '>' + View.template(Datas) + '</div>');
+
+                //initialize the new page
+                $.mobile.initializePage();
+            }
+        }
+        
+        ChangePage(View, options)
+        {
+            Backbone.history.navigate(View, true);
+        }
+        
+        ReloadPage()
+        {
+            Backbone.history.loadUrl();
+        }
+        
+        UnloadWebsite(result)
+        {
+            $(location).attr('href', app.URL);
+        }
+        
+        DisplayError(errorMessage)
+        {
+            alert(errorMessage);
+        }
+        
+        KeepSessionAlive()
+        {
+            $.ajax({url: app.API + 'Utility/KeepSessionAlive',
+                    dataType: 'json'
+            });
+        }
+        
+        DateFromMysql(mysql_string)
+        {
+            var t, result = null;
+
+            if( typeof mysql_string === 'string' )
+            {
+                t = mysql_string.split(/[- :]/);
+
+                //when t[3], t[4] and t[5] are missing they defaults to zero
+                result = new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
+            }
+
+           return result;
+        }
     }
 
-    return (window.MyPOS = MyPOS);
+    return (window.MyPOS = new MyPOS);
 
 } );
--- src/public/js/Webservice.js
+++ src/public/js/Webservice.js
@@ -2,60 +2,58 @@
 // ----------
 
 //Includes file dependencies
-define(["app", "jquery"], function(app) {
+define(["app"], function(app) {
     "use strict";
+    
+    return class Webservice {
+        constructor(action, callback, formData, dataType) {
+            _.bindAll(this, "success", "error");
 
-    function Webservice(action, callback, formData, dataType)
-    {
-        _.bindAll(this, "success", "error");
+            if(typeof dataType === 'undefined') dataType = 'json';
 
-        if(typeof dataType === 'undefined') dataType = 'json';
-
-        this.action = action;
-        this.async = true;
-        this.formData = formData;
-        this.callback = callback;
-        this.dataType = dataType;
-    }
-
-    Webservice.prototype.call = function()
-    {
-        $.ajax({url: app.API + this.action + '/',
+            this.action = action;
+            this.async = true;
+            this.formData = formData;
+            this.callback = callback;
+            this.dataType = dataType;
+        }
+        
+        call() {
+            $.ajax({url: app.API + this.action,
                 data: this.formData,
-                type: 'post',
+                type: 'POST',
                 async: this.async,
                 dataType: this.dataType,
                 success: this.success,
                 error: this.error,
                 timeout: ((DEBUG) ? 0 : 30000)
-        });
-    }
-
-    Webservice.prototype.success = function(result)
-    {
-        if(result.error)
-        {
-            this.DisplayError(result.errorMessage);
-            if(this.callback && 'error' in this.callback) this.callback.error(result);
+            });
         }
-        else
+        
+        success(result)
         {
-            if(this.callback && 'success' in this.callback) this.callback.success(result.result);
+            if(result.error)
+            {
+                this.DisplayError(result.errorMessage);
+                if(this.callback && 'error' in this.callback) this.callback.error(result);
+            }
+            else
+            {
+                if(this.callback && 'success' in this.callback) this.callback.success(result.result);
+            }
+
+            if(this.callback && 'complete' in this.callback) this.callback.complete();
         }
-
-        if(this.callback && 'complete' in this.callback) this.callback.complete();
+        
+        error(jqXHR, textStatus, errorThrown)
+        {
+            this.DisplayError(textStatus + ": " + errorThrown);
+            if('ajaxError' in this.callback) this.callback.ajaxError(jqXHR, textStatus, errorThrown);
+        }
+        
+        DisplayError(errorMessage)
+        {
+            alert(errorMessage);
+        }
     }
-
-    Webservice.prototype.error = function(jqXHR, textStatus, errorThrown)
-    {
-        this.DisplayError(textStatus + ": " + errorThrown);
-        if('ajaxError' in this.callback) this.callback.ajaxError(jqXHR, textStatus, errorThrown);
-    }
-
-    Webservice.prototype.DisplayError = function(errorMessage)
-    {
-        alert(errorMessage);
-    }
-
-    return Webservice;
 } );
