52e18b677a4b1c5b975a48a4bd4d6ebcd66e091e
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-10-16 14:42:54 +0200

    removed unsued root var

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -10,9 +10,8 @@ function($, _, Backbone) {
     "use strict";
 
     var app = {
-        root : "/",                     // The root path to run the application through.
         URL : "/",                      // Base application URL
-        API : "api/",                   // Base API URL (used by models & collections)
+        API : "API/",                   // Base API URL (used by models & collections)
     };
 
     $.ajaxSetup({ cache: false });      // force ajax call on all browsers
