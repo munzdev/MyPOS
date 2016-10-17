2c2a9d31a377ae71945f8d0edcae0543fefd7437
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-10-17 23:13:00 +0200

    - refactored to use .Promise() - opimized code

--- src/public/js/Webservice.js
+++ src/public/js/Webservice.js
@@ -6,54 +6,27 @@ define(["app"], function(app) {
     "use strict";
     
     return class Webservice {
-        constructor(action, callback, formData, dataType) {
-            _.bindAll(this, "success", "error");
-
-            if(typeof dataType === 'undefined') dataType = 'json';
-
+        constructor(action, formData = null, type = 'POST', dataType = 'json') {
             this.action = action;
             this.async = true;
+            this.type = type;
             this.formData = formData;
-            this.callback = callback;
             this.dataType = dataType;
         }
         
         call() {
-            $.ajax({url: app.API + this.action,
+            
+            if(window["DEBUG"] == undefined) var DEBUG = false;
+            
+            return $.ajax({url: app.API + this.action,
                 data: this.formData,
-                type: 'POST',
+                type: this.type,
                 async: this.async,
                 dataType: this.dataType,
                 success: this.success,
                 error: this.error,
                 timeout: ((DEBUG) ? 0 : 30000)
             });
-        }
-        
-        success(result)
-        {
-            if(result.error)
-            {
-                this.DisplayError(result.errorMessage);
-                if(this.callback && 'error' in this.callback) this.callback.error(result);
-            }
-            else
-            {
-                if(this.callback && 'success' in this.callback) this.callback.success(result.result);
-            }
-
-            if(this.callback && 'complete' in this.callback) this.callback.complete();
-        }
-        
-        error(jqXHR, textStatus, errorThrown)
-        {
-            this.DisplayError(textStatus + ": " + errorThrown);
-            if('ajaxError' in this.callback) this.callback.ajaxError(jqXHR, textStatus, errorThrown);
-        }
-        
-        DisplayError(errorMessage)
-        {
-            alert(errorMessage);
         }
     }
 } );
