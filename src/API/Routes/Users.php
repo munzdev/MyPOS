a2db24fe7f71af7c4d8ed037fd0640d6fd7d2a3f
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-10-28 10:40:20 +0200

    removed not needed route

--- src/API/Routes/Users.php
+++ src/API/Routes/Users.php
@@ -3,6 +3,4 @@
 $app->group('/Users', function () {
     $this->any('/', new API\Controllers\Users\Users($this))
          ->setName('Users');
-    $this->any('/Current', new API\Controllers\Users\Users($this))
-         ->setName('Users-Current');
 });
