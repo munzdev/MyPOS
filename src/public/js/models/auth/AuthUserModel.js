1e61e27e88d2c8a557580146e858161f48a6ef62
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-10-29 08:52:34 +0200

    fixed directory name

---
+++ src/public/js/models/auth/AuthUserModel.js
@@ -0,0 +1,20 @@
+define([
+    "app"
+], function(app){
+    "use strict";
+    
+    return class AuthUserModel extends Backbone.Model
+    {
+        urlRoot() { return app.API + "Login/User"; }
+        defaults() {
+            return {
+                Userid: 0,
+                Username: '',
+                Firstname: '',
+                Lastname: '',
+                IsAdmin: 0,
+                UserRoles: 0
+            }
+        }
+    }
+});
