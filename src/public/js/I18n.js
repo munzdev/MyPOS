53f4c343303dd4d8894cb163dbf748cb24c2f512
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-11-03 12:03:39 +0100

    optimized i18n lib - removed duplicates

--- src/public/js/I18n.js
+++ src/public/js/I18n.js
@@ -7,23 +7,19 @@ define(['module'], function(module) {
     
     return class I18n {
         constructor(externalDoneCallback) {
+            _.bindAll(this, "storeTemplate");
+            
             this.externalDoneCallback = externalDoneCallback;
             this.language = window.navigator.userLanguage || window.navigator.language;
             
             $.getJSON(require.toUrl('i18n/' + this.language + '.json'))
-                    .done((json) => {
-                        this.template = json;
-                        this.externalDoneCallback();
-                    })
+                    .done(this.storeTemplate)
                     .fail(() => {                        
                         if(this.language.length > 2)
                         {
                             this.language = this.language.substr(0, 2);
                             this.template = $.getJSON(require.toUrl('i18n/' + this.language + '.json'))
-                                    .done((json) => {
-                                        this.template = json;
-                                        this.externalDoneCallback();
-                                    })
+                                    .done(this.storeTemplate)
                                     .fail(() => {
                                         this.loadFallbackLanguage();
                                     });
@@ -35,13 +31,16 @@ define(['module'], function(module) {
                     });
         }
         
+        storeTemplate(json)
+        {
+            this.template = json;
+            this.externalDoneCallback();
+        }
+        
         loadFallbackLanguage()
         {
             this.template = $.getJSON(require.toUrl('i18n/en.json'))
-                    .done((json) => {
-                        this.template = json;
-                        this.externalDoneCallback();
-                    })
+                    .done(this.storeTemplate)
                     .error((xHR, error) => 
                     {
                         alert("Language file couldnt' be loaded! Please reload the App! Error: " + error);
