2cf6815b4d4702e8696cd01289407a60b1d89f62
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-11-04 11:16:57 +0100

    use recursive search to scan route files

--- src/API/routes.php
+++ src/API/routes.php
@@ -1,7 +1,14 @@
 <?php
 // Routes
 
-foreach (glob(__DIR__ . "/Routes/*.php", GLOB_NOSORT) as $str_filename)
+$o_directory = new RecursiveDirectoryIterator(__DIR__ . "/Routes/", FilesystemIterator::KEY_AS_PATHNAME | 
+                                                                    FilesystemIterator::CURRENT_AS_FILEINFO |
+                                                                    FilesystemIterator::SKIP_DOTS);
+
+$o_iterator = new RecursiveIteratorIterator($o_directory);
+
+foreach($o_iterator as $str_filename => $o_file)
 {
-    require $str_filename;
+    if ($o_file->isFile())
+        require $str_filename;
 }
