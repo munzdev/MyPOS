5a9ce42a816533dcbcb9135d968caf2cc9166027
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-11-27 19:36:35 +0100

    added missing customer Table model

---
+++ src/public/js/models/db/Invoice/Customer.js
@@ -0,0 +1,33 @@
+define(["models/db/Event/Event"        
+], function(Event){
+    "use strict";
+
+    return class Customer extends app.BaseModel {
+        
+        idAttribute() { return 'Customerid'; }
+
+        defaults() {
+            return {Customerid: null,
+                    Eventid: null,
+                    Title: '',
+                    Name: '',
+                    Adress: '',
+                    Adress2: '',
+                    City: '',
+                    Zip: '',
+                    TaxIdentifictionNr: '',
+                    Active: false};
+        }
+        
+        parse(response)
+        {
+            if('Event' in response)
+            {
+                response.Event = new Event(response.Event, {parse: true});
+            }
+            
+            return super.parse(response);
+        }
+
+    }
+});
