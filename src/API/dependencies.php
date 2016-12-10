2459e6eab4d6f08c52ac3e635988e9961b8a6040
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-12-10 10:31:16 +0100

    fixed namespace usage

--- src/API/dependencies.php
+++ src/API/dependencies.php
@@ -1,4 +1,9 @@
 <?php
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
+use const API\PUBLIC_ROOT;
 // DIC configuration
 
 $o_container = $app->getContainer();
@@ -13,9 +18,9 @@ $o_container = $app->getContainer();
 // monolog
 $o_container['logger'] = function ($o_container) {
     $settings = $o_container->get('settings');
-    $logger = new Monolog\Logger($settings['logger']['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['logger']['path'], Monolog\Logger::DEBUG));
+    $logger = new Logger($settings['logger']['name']);
+    $logger->pushProcessor(new UidProcessor());
+    $logger->pushHandler(new StreamHandler($settings['logger']['path'], Logger::DEBUG));
     return $logger;
 };
 
@@ -23,10 +28,10 @@ $o_container['logger'] = function ($o_container) {
 $o_container['i18n'] = function($o_container) {
     $str_lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
     
-    $str_file = \API\PUBLIC_ROOT . 'js/i18n/' . $str_lang . '.json';
+    $str_file = PUBLIC_ROOT . 'js/i18n/' . $str_lang . '.json';
     
     if(!file_exists($str_file)) 
-        $str_file = \API\PUBLIC_ROOT . 'js/i18n/en.json';    
+        $str_file = PUBLIC_ROOT . 'js/i18n/en.json';    
     
     $str_json = file_get_contents($str_file);
     return json_decode($str_json);
