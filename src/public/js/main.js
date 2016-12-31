1623ade37aff9755cac3f68fe03eb4753d282766
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2016-12-31 14:07:43 +0100

    added lib sprintf.js

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,8 +1,8 @@
 "use strict";
-    
+
 // Sets the require.js configuration for your application.
 require.config( {
-    
+
     // 3rd party script alias names (Easier to type "jquery" than "libs/jquery-1.8.3.min")
     paths: {
         // Core Libraries
@@ -15,6 +15,7 @@ require.config( {
         "backbone": "libs/backbone/backbone",
         "text": "libs/require/text",
         "wampy": "libs/wampy/wampy",
+        "sprintf": "libs/sprintf/sprintf",
 
         // Core Directorys
         "templates": "../templates",
@@ -35,7 +36,7 @@ require.config( {
             "exports": "Backbone"  //attaches "Backbone" to the window object
         }
     } // end Shim Configuration
-    
+
 } );
 
 // Includes File Dependencies
@@ -45,7 +46,7 @@ require([ "I18n",
           "backbone",
           "jquerymobile"],
 function(I18n) {
-             
+
     // Disabling this will prevent jQuery Mobile from handling hash changes
     $.mobile.hashListeningEnabled = false; // Disable hash listing as Backbone will handle changes
     $.mobile.ajaxEnabled = false; // Dont use Ajax calls
@@ -53,12 +54,12 @@ function(I18n) {
     $.mobile.changePage.defaults.changeHash = false;  // Don't change the URLS hash on link clicking. Will be done manuly by code events
     $.mobile.autoInitializePage = false; // As no Page is in the DOM by default, don't try to init them
     $.ajaxSetup({ cache: false });      // force ajax call on all browsers, no caching
-             
+
     var app = window.app = {
         URL : "/",                      // Base application URL
         API : "API/",                   // Base API URL (used by models & collections)
     };
-    
+
     // Enable Multi Language module
     app.i18n = new I18n(() => {
         require(["loadBaseClasses"]);
