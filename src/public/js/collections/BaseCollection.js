ff23e3549551048f26c5174dbb45999fb953d683
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2017-02-02 11:19:40 +0100

    dirty hack: In case of dependencie vialation/recursion, model will be empty. Try to fix it

--- src/public/js/collections/BaseCollection.js
+++ src/public/js/collections/BaseCollection.js
@@ -1,14 +1,20 @@
 define(function(){
     "use strict";
-    
+
     return class BaseCollection extends Backbone.Collection
     {
          constructor(models, options) {
             super(null, options);
 
             this.model = this.getModel();
-            
+
+            // dirty hack: In case of dependencie vialation/recursion, model will be empty. Try to fix it
+            if(this.model == undefined) {
+                this.model = app.BaseModel;
+                this.model.extend({idAttrribute: false});
+            }
+
             if (models) this.reset(models, _.extend({silent: true}, options));
-        }             
+        }
     }
 });
