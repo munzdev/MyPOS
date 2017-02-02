e47cba5e1bc25c854df770bb61145b7c12e43fe7
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2017-02-02 11:17:38 +0100

    fixed user column names

--- src/public/js/models/db/Ordering/Order.js
+++ src/public/js/models/db/Ordering/Order.js
@@ -10,7 +10,7 @@ define([
     "use strict";
 
     return class Order extends app.BaseModel {
-        
+
         idAttribute() { return 'Orderid'; }
 
         defaults() {
@@ -22,35 +22,40 @@ define([
                     DistributionFinished: null,
                     InvoiceFinished: null};
         }
-        
+
         parse(response)
         {
             if('EventTable' in response)
             {
                 response.EventTable = new EventTable(response.EventTable, {parse: true});
             }
-            
-            if('User' in response)
+
+            if('UserRelatedByUserid' in response)
             {
-                response.User = new User(response.User, {parse: true});
+                response.UserRelatedByUserid = new User(response.UserRelatedByUserid, {parse: true});
             }
-            
+
+            if('UserRelatedByCancellationCreatedByUserid' in response)
+            {
+                response.UserRelatedByCancellationCreatedByUserid = new User(response.UserRelatedByCancellationCreatedByUserid, {parse: true});
+            }
+
             if('OrderInProgresses' in response)
-            {   
+            {
                 if(response.OrderInProgresses.toString() == '')
                     response.OrderInProgresses = new OrderInProgressCollection();
                 else
                     response.OrderInProgresses = new OrderInProgressCollection(response.OrderInProgresses, {parse: true});
             }
-            
+
             if('OrderDetails' in response)
-            {                
+            {
                 if(response.OrderDetails.toString() == '')
                     response.OrderDetails = new OrderDetailCollection();
                 else
                     response.OrderDetails = new OrderDetailCollection(response.OrderDetails, {parse: true});
             }
-            
+
             return super.parse(response);
         }
 
