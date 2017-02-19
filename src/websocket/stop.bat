62b27ba6dbf094ad7ed3c17919ef8944ae4b7d21
Thomas Munz <Thomas@DESKTOP-09VP7QF>
2017-02-19 11:04:16 +0100

    removed old dir

--- src/websocket/stop.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-FOR /F %%T IN ('wmic process where^(commandline like "%%server.php%%"^) get ProcessId^|more +1') DO (
-SET /A ProcessId=%%T) &GOTO SkipLine
-:SkipLine
-taskkill /F /PID %ProcessId%
