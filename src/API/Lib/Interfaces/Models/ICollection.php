602dfb0e435ed068f621d95cf5a200ea36d7a215
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2017-04-14 23:18:05 +0200

    update collection interface to match requiments

--- src/API/Lib/Interfaces/Models/ICollection.php
+++ src/API/Lib/Interfaces/Models/ICollection.php
@@ -6,4 +6,20 @@ interface ICollection extends \ArrayAccess, \IteratorAggregate, \Countable, \Ser
     function toArray();
     function isEmpty();
     function getFirst();
+    function getLast();
+    function get($key);
+    function set($key, $value);
+    function remove($key);
+    function pop();
+    function shift();
+    function push($value);
+    function prepend($value);
+    function clear();
+    function contains($element);
+    function search($element);
+    function append($value);
+    function exchangeArray($input);
+    function setData($data);
+    function getData();
+    function getArrayCopy();
 }
