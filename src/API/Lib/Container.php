feac1f2a93ae4755ed5284699445808aa146a5a6
Thomas Munz <Thomas@DESKTOP-P7VALLD>
2017-04-15 10:32:59 +0200

    fixed serialization prolem of closures - hack that will on unserialize clone the singleton object via reflection

--- src/API/Lib/Container.php
+++ src/API/Lib/Container.php
@@ -2,19 +2,27 @@
 
 namespace API\Lib;
 
+use Serializable;
 use Slim\Container as SlimContainer;
 use Slim\Exception\ContainerException as SlimContainerException;
 
-class Container extends SlimContainer
+class Container extends SlimContainer implements Serializable
 {
     private $registeredServices = [];
+
+    private static $static;
+
+    function __construct(array $values = array())
+    {
+        parent::__construct($values);
+        self::$static = $this;
+    }
 
     public function registerService(string $id, callable $callback, string $interface = null)
     {
         if($interface == null) {
             $interface = $id;
         }
-        #
 
         if(!isset($this->registeredServices[$id])) {
             $this->registeredServices[$id] = $interface;
@@ -36,6 +44,54 @@ class Container extends SlimContainer
         }
 
         return parent::get($id);
+    }
+
+    public function serialize(): string
+    {
+        return "";
+    }
+
+    public function unserialize($serialized): void
+    {
+        $reflectionLibContainer = new \ReflectionClass(self::$static);
+        $reflectionSlimContainer = $reflectionLibContainer->getParentClass();
+        $reflectionPimpleContainer = $reflectionSlimContainer->getParentClass();
+
+        $propertiesLibContainer = $reflectionLibContainer->getProperties();
+        $propertiesPimpleContainer = $reflectionPimpleContainer->getProperties();
+
+        $reflectionNewLib = new \ReflectionClass($this);
+        $reflectionNewSlim = $reflectionNewLib->getParentClass();
+        $reflectionNewPimple = $reflectionNewSlim->getParentClass();
+
+
+        foreach ($propertiesLibContainer as $property) {
+            if ($property->getName() == 'static') {
+                continue;
+            }
+
+            $propertyLib = $reflectionLibContainer->getProperty($property->getName());
+            $propertyLib->setAccessible(true);
+            $value = $propertyLib->getValue(self::$static);
+            $propertyLib->setAccessible(false);
+
+            $propertyNew = $reflectionNewLib->getProperty($property->getName());
+            $propertyNew->setAccessible(true);
+            $propertyNew->setValue($this, $value);
+            $propertyNew->setAccessible(false);
+        }
+
+        foreach ($propertiesPimpleContainer as $property) {
+            $propertyPimple = $reflectionPimpleContainer->getProperty($property->getName());
+            $propertyPimple->setAccessible(true);
+            $value = $propertyPimple->getValue(self::$static);
+            $propertyPimple->setAccessible(false);
+
+            $propertyNew = $reflectionNewPimple->getProperty($property->getName());
+            $propertyNew->setAccessible(true);
+            $propertyNew->setValue($this, $value);
+            $propertyNew->setAccessible(false);
+        }
     }
 
 }
