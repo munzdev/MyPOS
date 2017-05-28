cb92b4e4d619c3f6926d17cfe7fb249fa7b5aa7c
Thomas Munz <Thomas@DESKTOP-09VP7QF>
2017-05-28 09:43:12 +0200

    fixed AdminController isAdmin Check

--- src/API/Lib/AdminController.php
+++ src/API/Lib/AdminController.php
@@ -3,13 +3,14 @@ namespace API\Lib;
 
 use API\Lib\Exceptions\SecurityException;
 use API\Lib\Interfaces\IAuth;
-use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 abstract class AdminController extends SecurityController
 {
-    public function __construct(App $app)
-    {
-        parent::__construct($app);
+    public function __invoke(Request $request, Response $response, $args) : Response
+    {        
+        $result = parent::__invoke($request, $response, $args);
 
         $auth = $this->container->get(IAuth::class);
         $user = $auth->getCurrentUser();
@@ -17,5 +18,7 @@ abstract class AdminController extends SecurityController
         if (!$user->getIsAdmin()) {
             throw new SecurityException("Access Denied!");
         }
+        
+        return $result;
     }
 }
